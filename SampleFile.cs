d76702ff827be27708cae18a02fcc818cc55a2e2
Brogers <brogers@mpifix.com>
2012-04-18 20:28:14 -0600

    Copyright headers applied by SRSWP Copyright service

--- SampleFile.cs
+++ SampleFile.cs
@@ -1,1 +1,5 @@
+// =====COPYRIGHT=====
+// Copyright 2007 - 2012 Service Repair Solutions, Inc.
+// Copyright 2007 - 2012 Service Repair Solutions, Inc.
+// =====COPYRIGHT=====
 //This is sample for CSharp file type
